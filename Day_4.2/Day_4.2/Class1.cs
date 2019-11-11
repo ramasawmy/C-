67fac4206bc6c5bd410a62b5390d34dffb9ae726
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-11 14:37:28 +0400

    Add files via upload

---
+++ Day_4.2/Day_4.2/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Day_4._2
+{
+    class Customer
+    {
+        public int CustomerID { get; set; }
+        public String Name { get; set; }
+        public string Email { get; set; }
+
+        public void Details()
+        {
+            Console.WriteLine("CustomerID " + CustomerID);
+        }
+
+    }
+
+    class BankCustomer:Customer
+    {
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+
+        public void BankCustomerDetails()
+        {
+            Console.WriteLine("CustomerID " + CustomerID);
+            Console.WriteLine("Bal " + Balance);
+        }
+    }
+    public class Class1
+    {
+        static void Main(String[] args)
+        {
+            Customer C = new Customer()
+            {
+                CustomerID = 123,
+                Name = "Steven",
+                Email = "Steven@gmail.com"
+            };
+
+            BankCustomer BC = new BankCustomer()
+            {
+                CustomerID = 124,
+                Name = "Peter",
+                Email = "Peter@gmail.com",
+                AccountNumber = 12345,
+                Balance = 5000
+
+            };
+
+            BC.Details();
+            BC.BankCustomerDetails();
+
+            C.Details();
+
+        }
+
+
+    }
+}
