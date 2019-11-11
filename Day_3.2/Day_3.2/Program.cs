1eaf4cb4fef9f25f0bd8a1318bffa0d74c2fe174
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-11 14:36:29 +0400

    Add files via upload

---
+++ Day_3.2/Day_3.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using MyOrg.HR;
+
+//namespace Admin
+//{
+//    class Employee
+//    {
+//        public void Add() { }
+//        public void Update() { }
+//    }
+//}
+
+namespace MyOrg
+{
+    namespace HR
+    {
+        class Employee
+        {
+            public void CalculateSalary() { }
+            public void CalculateLeave() { }
+        }
+    }
+}
+
+namespace Day_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //admin.employee e1 = new admin.employee();
+            //e1.add();
+            //e1.update();
+
+            //HR.Employee E2 = new HR.Employee();
+            //E2.CalculateSalary();
+            //E2.CalculateLeave();
+
+            //MyOrg.HR.Employee E1 = new MyOrg.HR.Employee();
+            //E1.CalculateSalary();
+
+            Employee E1 = new Employee();
+            E1.CalculateSalary();
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
