ee2961d4db166518929ec75d33c22f481e867d4b
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-11 14:38:08 +0400

    Add files via upload

---
+++ InheritanceEg1/InheritanceEg1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace InheritanceEg1
+{
+    class Customer
+    {
+        public int CustomerID { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+
+        public void Details()
+        {
+            Console.WriteLine("CustomerID " + CustomerID);
+        }
+    }
+
+    class BankCustomer:Customer
+    {
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+
+        public void BankCustomerDetails()
+        {
+            Console.WriteLine("CustomerID " + CustomerID);
+            Console.WriteLine("Bal " + Balance );
+        }
+    }
+
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Customer C = new Customer()
+            {
+                CustomerID = 123,
+                Name = "Peter",
+                Email = "Peter@gmail.com"
+            };
+
+            BankCustomer BC = new BankCustomer()
+            {
+                CustomerID = 124,
+                Name = "Jack",
+                Email = "Jack@gmail.com",
+                AccountNumber = 12345,
+                Balance = 5000,
+            };
+
+            BC.Details();
+            BC.BankCustomerDetails();  
+
+            C.Details();
+
+        }
+    }
+}
