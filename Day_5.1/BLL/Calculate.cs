427208d8d3ea414c0e39b7f989e2c14e0cf4a89a
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-12 11:18:32 +0400

    Add files via upload

---
+++ Day_5.1/BLL/Calculate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BLL
+{
+    public class Calculate
+    {
+        public static double Divide(int a, int b)
+        {
+            try
+            {
+                if (b !=0)
+                {
+                    return a / b;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            
+            catch(Exception E)
+            {
+                throw new Exception("Invalid second param or b value cannot be zero");
+
+
+            }
+
+        }
+    }
+}
---
+++ Day_5.1/Day_5.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using BLL;
+using System;
+
+namespace Day_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var result = Calculate.Divide(15, 0);
+                Console.WriteLine("Result is " + result);
+            }
+            catch (Exception E)
+            {
+
+                Console.WriteLine(E.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
