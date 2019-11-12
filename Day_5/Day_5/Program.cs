8d4bf8b474533c04ca15d11264548ad1412f6c7b
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-12 10:19:21 +0400

    Add files via upload

---
+++ Day_5/Day_5/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+
+namespace Day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Collection class
+            //Stack s = new Stack(3);
+            //s.Push(2);
+            //s.Push(8);
+            //s.Push(8);
+
+            //while(s.Count !=0)
+            //{
+            //    Console.WriteLine(s.Pop().ToString());
+            //}
+
+            //int sum = 0;
+            //while (s.Count !=0)
+            //{
+            //    sum = sum + int.Parse(s.Pop().ToString());
+            //}
+            //Console.WriteLine("Sum is " + sum);
+
+            //Queue q = new Queue();
+            //q.Enqueue("lolo");
+            //q.Enqueue("lala");
+            //q.Enqueue("lili");
+
+            ////while (q.Count != 0)
+            ////    {
+            ////       Console.WriteLine(q.Dequeue().ToString());
+            ////    }
+
+            //string name = null;
+            //while (q.Count !=0)
+            //{
+            //    name = name + " " + q.Dequeue();
+            //}
+            //Console.WriteLine(name);
+
+            #endregion
+            #region TryCatch
+            //int[] a = new int[2];
+            //try
+            //{
+
+
+            //    a[0] = 0;
+            //    Console.WriteLine(a[0]);
+
+            //    a[1] = int.Parse("xyz");
+            //    Console.WriteLine(a[1]);
+
+            //    var x = a[1] / a[0];
+
+            //    //a[2] = 7;
+            //    //Console.WriteLine(a[2]);
+
+            //}
+            //catch(FormatException FE)
+            //{
+            //    Console.WriteLine(FE.Message);
+            //    a[1] = 0;
+            //}
+            //catch (DivideByZeroException DE)
+            //{
+            //    Console.WriteLine(DE.Message);
+            //}
+            //catch (IndexOutOfRangeException IE)
+            //{
+            //    Console.WriteLine(IE.Message);
+            //}
+            //catch (Exception E)
+            //{
+            //    Console.WriteLine(E.Message);
+            //}
+            //finally { } 
+            #endregion
+            #region TRY CATCH
+            //try
+            //{
+            //    int[] a = new int[2];
+            //    a[0] = int.Parse(Console.ReadLine());
+            //    a[1] = 12 / a[0];
+            //}
+
+            //catch (Exception E)
+            //{
+            //    Console.WriteLine(E.Message);
+            //}
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("steven");
+            //} 
+            #endregion
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
