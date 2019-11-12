bb41baab095655210a8d134406873ff3e76fabfd
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-12 17:38:48 +0400

    Add files via upload

---
+++ Product DLL/Creating Product Dll part 2/Creating Product Dll part 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Creating_Product_Dll;
+
+namespace Creating_Product_Dll_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ppu;
+            int nou;
+            double ba;
+
+            Console.WriteLine("Enter Price Per Unit");
+            ppu = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter No Of Unit");
+            nou = int.Parse(Console.ReadLine());
+
+            Product p = new Product();
+
+            ba = p.GetBillAmount(ppu, nou);
+
+            Console.WriteLine("Your bill Amount is " + ba);
+
+            Console.ReadLine();
+        }
+    }
+}
