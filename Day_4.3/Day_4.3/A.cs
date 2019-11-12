708cdfd9c7d38c2200e8da035f172401981ebd6d
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-12 16:58:06 +0400

    Add files via upload

---
+++ Day_4.3/Day_4.3/A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_4._3
+{
+    partial class A
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display");
+        }
+
+    }
+}
---
+++ Day_4.3/Day_4.3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Day_4._3
+{
+    #region Interface
+    //interface IBulkOperation
+    //{
+    //    void DeleteAll();       
+    //}
+    //interface Ioperations
+    //{
+    //    void GetAll();
+    //    void GetByTD();
+    //    void create();
+    //    void Update();
+    //    void Delete();
+
+    //}
+
+    //class Employee : Ioperations, IBulkOperation
+    //{
+    //    public void create()
+    //    {
+    //        Console.WriteLine("Create Employee");
+    //    }
+
+    //    public void Delete()
+    //    {
+    //        Console.WriteLine("Delete Employee");
+    //    }
+
+    //    public void DeleteAll()
+    //    {
+    //        Console.WriteLine("Delete All Operation");
+    //    }
+
+    //    public void GetAll()
+    //    {
+    //        Console.WriteLine("Get all Employee");
+    //    }
+
+    //    public void GetByTD()
+    //    {
+    //        Console.WriteLine("Get Employee ID");
+    //    }
+
+    //    public void Update()
+    //    {
+    //        Console.WriteLine("Update Employee");
+    //    }
+    //} 
+    #endregion
+
+    partial class A
+    {
+        public void show()
+        {
+            Console.WriteLine("show");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+
+            a.show();
+            a.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
