a1d13a4262b85efce81dac653cc185ec421814e8
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-14 12:20:56 +0400

    Add files via upload

---
+++ Creating Product Dll Part 3/Creating Product Dll Part 3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Creating_Product_Dll;
+
+namespace Creating_Product_Dll_Part_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            int ppu = int.Parse(textBox1.Text);
+            int nou = int.Parse(textBox2.Text);
+            double ba = p.GetBillAmount(ppu, nou);
+
+           label1.Text = ba.ToString();
+
+
+
+        }
+    }
+}
---
+++ Creating Product Dll/Creating Product Dll/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_Product_Dll
+{
+    public class Product
+    {
+        public double  GetBillAmount(int PricePerUnit, int NoOfUnits)
+        {
+            double amt;
+            amt = PricePerUnit * NoOfUnits;
+            return amt;
+
+        }
+
+    }
+}
