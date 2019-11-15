35e12e813d42cf9e5196eb9a4fe1fd42a2ae37b6
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-15 14:10:12 +0400

    Add files via upload

---
+++ Interface Segregatoin Principle/Interface Segregatoin Principle/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Interface_Segregatoin_Principle
+{
+    public class Document
+    {
+
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MaultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+        public void Scan(Document d)
+        {
+            //
+        }
+        public void Fax(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFashionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionDevice : IScanner, IPrinter
+    {
+
+    }
+
+    public class MultiFunctionMachine : IMultiFunctionDevice
+    {
+        private IPrinter Printer;
+        private IScanner Scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            this.Printer = printer;
+            this.Scanner = scanner;
+        }
+        public void Print(Document d)
+        {
+            Printer.Print(d);
+        }
+        public void Scan(Document d)
+        {
+            Scanner.Scan(d);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Liskov Subtitution Principle/Liskov Subtitution Principle/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Liskov_Subtitution_Principle
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+            
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public new int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public new int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+
+
+
+    public class Program
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle();
+            WriteLine($"{rc} has area {Area(rc)}");
+
+            Square sq = new Square();
+            sq.Width = 4;
+            WriteLine($"{ sq} has area { Area(sq)}");
+
+            Console.ReadLine();
+        }
+    }
+}
