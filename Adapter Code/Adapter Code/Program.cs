bd49806419ccc6fc593ca39fb59f6b121ebae576
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-19 16:45:58 +0400

    Add files via upload

---
+++ Adapter Code/Adapter Code/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Adapter_Code
+{
+    /// <summary>
+    /// MainApp startup class for Structural
+    /// Adapter Design Pattern.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Create adapter and place a request
+            Target target = new Adapter();
+            target.Request();
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Target' class
+    /// </summary>
+    class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Adapter' class
+    /// </summary>
+    class Adapter : Target
+    {
+        private Adaptee _adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            // Possibly do some other work
+            //  and then call SpecificRequest
+            _adaptee.SpecificRequest();
+        }
+    }
+
+    /// <summary>
+    /// The 'Adaptee' class
+    /// </summary>
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+    }
+
+
+
+
+
+
+}
