a6bdfdd46b6860546618e2a56b891afa13e390e9
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-19 17:01:24 +0400

    Add files via upload

---
+++ Singleton Real World Code/Singleton Real World Code/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Singleton_Real_World_Code
+{
+    /// <summary>
+    /// MainApp startup class for Real-World 
+    /// Singleton Design Pattern.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same Instance\n");
+            }
+
+            // Load balance 15 server request
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                String server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private List<string> _servers = new List<string>();
+        private Random _random = new Random();
+
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        // Constructor (Protected)
+        protected LoadBalancer()
+        {
+            // List of available servers
+            _servers.Add("Server-I");
+            _servers.Add("Server-II");
+            _servers.Add("Server-III");
+            _servers.Add("Server-IV");
+            _servers.Add("Server-V");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            // Support multithreaded applications through
+            // 'Double checked locking' pattern which (once
+            // the instance exists) avoids locking each
+            // time the method is invoked
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LoadBalancer();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        // Simple, but effective random load balancer
+        public String Server
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r].ToString();
+            }
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
