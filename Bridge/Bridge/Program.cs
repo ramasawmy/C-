88934952e4dff0be994d22c2d44ef927e04347f8
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-19 16:49:35 +0400

    Add files via upload

---
+++ Bridge/Bridge/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+    }
+
+   
+
+    public class Square : Shape
+    {
+        public Square()
+        {
+            Name = "Square";
+        }
+    }
+
+   
+    
+
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public abstract class shape
+    {
+        private IRenderer rendering;
+
+        protected shape(IRenderer rendering)
+        {
+            this.rendering = rendering;
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Drawing {Name} as {rendering.WhatToRenderAs}";
+        }
+    }
+
+    public class triangle : Shape
+    {
+        public triangle(IRenderer strategy) : base()
+        {
+            Name = "Triangle";
+        }
+    }
+
+    public class square : Shape
+    {
+        public square(IRenderer rendering) : base()
+        {
+            Name = "Square";
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public string WhatToRenderAs
+        {
+            get { return "pixels"; }
+        }
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public string WhatToRenderAs
+        {
+            get { return "lines"; }
+        }
+    }
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+           
+        }
+    }
+
+
+}
+
