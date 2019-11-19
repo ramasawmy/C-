be8346f340be69dd8bf81298eedacd992bd8b571
Steven Ramasawmy <52348786+ramasawmy@users.noreply.github.com>
2019-11-19 16:52:43 +0400

    Add files via upload

---
+++ Factory Coding Exercise/Factory Coding Exercise/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coding.Exercise
+
+{
+
+    public class Person
+
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public static PersonFactory Factory = new PersonFactory();
+
+
+
+        internal Person() { }
+
+
+
+        public override string ToString()
+
+        {
+
+            return $"Id: {Id}, Name: {Name}";
+
+        }
+
+    }
+
+
+
+    public class PersonFactory
+
+    {
+
+        public List<Person> People = new List<Person>();
+
+        private static int _id = 0;
+
+
+
+        public Person CreatePerson(string name)
+
+        {
+
+            Person p = new Person { Name = name, Id = _id++ };
+
+            People.Add(p);
+
+            return p;
+
+        }
+
+
+
+        public override string ToString()
+
+        {
+
+            var result = new StringBuilder();
+
+            foreach (var p in People)
+
+            {
+
+                result.AppendLine(p.ToString());
+
+            }
+
+            return result.ToString();
+
+        }
+
+
+
+    }
+
+}
---
+++ factory coding/factory coding/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoryExercise
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static PersonFactory Factory = new PersonFactory();
+
+        private Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Name}";
+        }
+
+        public class PersonFactory
+        {
+            private int CurrentId = 0;
+            public Person CreatePerson(string name)
+            {
+                return new Person(CurrentId++, name);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            persons.Add(Person.Factory.CreatePerson("Mark"));
+            persons.Add(Person.Factory.CreatePerson("John"));
+            persons.Add(Person.Factory.CreatePerson("Jack"));
+
+            foreach (Person p in persons) Console.WriteLine(p);
+
+            Person.PersonFactory factory = new Person.PersonFactory();
+            persons.Clear();
+
+            persons.Add(factory.CreatePerson("Mary"));
+            persons.Add(factory.CreatePerson("Jane"));
+            persons.Add(factory.CreatePerson("Judy"));
+
+            foreach (Person p in persons) Console.WriteLine(p);
+
+            Console.ReadKey();
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
